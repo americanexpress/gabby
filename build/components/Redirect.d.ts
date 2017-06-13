a2f4de6878af3563c932e6ce3e5d6f2d8c53a035
Zach <zachary.j.ripka@aexp.com>
2017-06-13 13:01:53 -0700

    Update typings.

---
+++ build/components/Redirect.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+import { StatelessComponent } from 'react';
+export interface IRedirectProps {
+    name: string;
+    when: string;
+    to: string;
+}
+declare const Redirect: StatelessComponent<IRedirectProps>;
+export default Redirect;
