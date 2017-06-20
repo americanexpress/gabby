8e51dd3632eb28203295d5523bf99e86db33c155
Zach <zachary.j.ripka@aexp.com>
2017-06-20 11:41:06 -0700

    Export components.

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,7 @@
 import Gab from './Gab';
 import parseReactRoutes from './parseReactRoutes';
-export { parseReactRoutes };
+import Root from './components/Root';
+import Route from './components/Route';
+import Redirect from './components/Redirect';
+export { parseReactRoutes, Root, Route, Redirect };
 export default Gab;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import Gab from './Gab';
 import parseReactRoutes from './parseReactRoutes';
 
+import Root from './components/Root';
+import Route from './components/Route';
+import Redirect from './components/Redirect';
+
 export {
   parseReactRoutes,
+  Root,
+  Route,
+  Redirect,
 };
 
 export default Gab;
